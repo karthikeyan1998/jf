91fa59f796f274c9ce8dc6161fa1f50e5a49c9f8
karthikeyan1998 <47741331+karthikeyan1998@users.noreply.github.com>
2019-03-25 16:28:31 +0530

    Create number.C

---
+++ number.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,a[10],c[10],i,multiplier;
+scanf("%d",&multiplier);
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+	scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+c[i]=a[i]*multiplier;	
+}
+for(i=0;i<n;i++)
+{
+printf("%d * %d =%d \n",multiplier,a[i],c[i]);	
+}
+}
+© 2019 GitHub, Inc.
